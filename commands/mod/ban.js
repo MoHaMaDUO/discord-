9221431f17a7ba3dbde29687518b982718eb1ed3
mrjoker85 <77496459+mrjoker85@users.noreply.github.com>
2021-02-06 09:17:07 +0330

    Update ban.js

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "This command kicks a member!",
     run : async(client, message, args) => {
         const target = message.mentions.users.first();
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`:face1: Permission Nadari`)
         if(target){
             const memberTarget = message.guild.members.cache.get(target.id);
             memberTarget.ban();
