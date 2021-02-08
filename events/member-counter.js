fa01e4a60f5cc07332a32de11615c84fd3a477cd
MoHaMaD <78649462+MoHaMaDUO@users.noreply.github.com>
2021-02-08 22:01:15 +0330

    Update member-counter.js

--- events/member-counter.js
+++ events/member-counter.js
@@ -4,7 +4,7 @@ module.exports = async (client) =>{
         const memberCount = guild.memberCount;
         const channel = guild.channels.cache.get(`805012260358455316`)
         channel.setName(`【👥】𝐌𝐞𝐦𝐛𝐞𝐫𝐬 : ${memberCount.toLocaleString()}`)
-    }, 50000)
+    }, 5000)
 }
 
 module.exports = async (client) =>{
@@ -13,5 +13,5 @@ module.exports = async (client) =>{
         const memberCount = guild.memberCount;
         const channel = guild.channels.cache.get(`805012266247258123`)
         channel.setName(`【👥】bot : ${client.count.toLocaleString()}`)
-    }, 50000)
+    }, 5000)
 }
